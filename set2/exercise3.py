adac431b1ade8476ceb8fb8a413de1a88da7d273
Daniella Cotterell <cotterelldani@gmail.com>
2023-08-16 17:21:27 +1000

    final commit

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -19,7 +19,7 @@ def is_odd(a_number):
 
     So if a_number modulo two is zero, then it's even.
     """
-    return None
+    return a_number % 2
 
 
 def fix_it(moves=True, should_move=True):
@@ -60,7 +60,10 @@ def loops_1a():
     return a list of 10 items, each one a string with exacly one star in it.
     E.g.: ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*']
     """
-    return None
+    star_list = []
+    for i in range(10):
+        star_list.append("*")
+    return star_list
 
 
 def loops_1c(number_of_items=5, symbol="#"):
@@ -73,7 +76,10 @@ def loops_1c(number_of_items=5, symbol="#"):
     Remember that you're being passed arguments here. Don't hard code the number
     or the symbol, let it be whatever it wants to be.
     """
-    return None
+    hash_list = []
+    for i in range(number_of_items):
+        hash_list.append(symbol)
+        return hash_list
 
 
 def loops_2_preview():
@@ -115,7 +121,13 @@ def loops_2():
             ['*', '*', '*', '*', '*', '*', '*', '*', '*', '*'],
           ]
     """
-    return None
+    feild = []
+    for i in range(10):
+        row = []
+        for j in range(10):
+            row.append("*")
+        feild.append(row)
+    return feild
 
 
 def loops_3():
@@ -139,7 +151,13 @@ def loops_3():
     TIP: notice that this needs to to return strings of numbers,
          so call str(number) to cast.
     """
-    return None
+    feild = []
+    for i in range(10):
+        row = []
+        for j in range(10):
+            row.append(str(i))
+        feild.append(row)
+    return feild
 
 
 def loops_4():
@@ -159,7 +177,17 @@ def loops_4():
       ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     ]
     """
-    return None
+
+
+def loops_4():
+    field = []
+    for i in range(10):
+        row = []
+        for j in range(10):
+            row.append(j)
+        field.append(row)
+    field.append(row)
+    return field
 
 
 def loops_5():
@@ -189,7 +217,13 @@ def loops_5():
         f"There are {num_bottles} green bottles"
     you'll come to see the pros and cons of each over time.
     """
-    return None
+    field = []
+    for i in range(10):
+        row = []
+        for j in range(5):
+            row.append("(i{}, j {})".format(i, j))
+        field.append(row)
+    return field
 
 
 def loops_6():
@@ -212,7 +246,13 @@ def loops_6():
     You can use a variable.
     TIP: look out for the starting condition.
     """
-    return None
+    pyrimd = []
+    for i in range(10):
+        row = []
+        for j in range(i + 1):
+            row.append(j)
+        pyrimd.append(row)
+    return pyrimd
 
 
 def loops_7():
@@ -236,7 +276,11 @@ def loops_7():
     This is a hard problem. Use lots of experimentation and draw
     lots of diagrams!
     """
-    return None
+    pyrimd = []
+    for i in range(5):
+        row = [[""] * (4 - i) + ["*"] * (2 * i - 1) + [""] * (4 - i)]
+        pyrimd.append(row)
+    return pyrimd
 
 
 if __name__ == "__main__":
